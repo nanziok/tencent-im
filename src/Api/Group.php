b68989f9cf1b8e78a5f2913377ab3157b67d5a44
mifan <596880630@qq.com>
2023-11-11 15:51:23 +0800

    namespace

--- src/Api/Group.php
+++ src/Api/Group.php
@@ -1,21 +1,21 @@
 <?php
 
-namespace Nanziok\TencentIM\Api;
+namespace QcloudIM\Api;
 
-use Nanziok\TencentIM\Constants;
-use Nanziok\TencentIM\Model\CreateGroupItem;
-use Nanziok\TencentIM\Model\GroupMemberInfoResponseFilter;
-use Nanziok\TencentIM\Model\GroupResponseFilter;
-use Nanziok\TencentIM\Model\ModifyGroupItem;
-use Nanziok\TencentIM\Model\ModifyGroupMemberInfoItem;
-use Nanziok\TencentIM\Traits\HttpClientTrait;
+use QcloudIM\Constants;
+use QcloudIM\Model\CreateGroupItem;
+use QcloudIM\Model\GroupMemberInfoResponseFilter;
+use QcloudIM\Model\GroupResponseFilter;
+use QcloudIM\Model\ModifyGroupItem;
+use QcloudIM\Model\ModifyGroupMemberInfoItem;
+use QcloudIM\Traits\HttpClientTrait;
 
 /**
  * 群组管理
  */
-class Group
-{
+class Group {
     use HttpClientTrait;
+
     /**
      * 获取 App 中的所有群组
      *
@@ -23,11 +23,10 @@ class Group
      *
      * @return array
      */
-    public function all_group($filter = []): array
-    {
-        if($filter){
+    public function all_group($filter = []): array {
+        if ($filter) {
             $r = $this->httpClient->postJson('group_open_http_svc/get_appid_group_list', $filter);
-        }else{
+        } else {
             $r = $this->httpClient->postJson('group_open_http_svc/get_appid_group_list');
         }
         if ($r['ErrorCode'] !== 0) {
@@ -35,6 +34,7 @@ class Group
         }
         return $r;
     }
+
     /**
      * 创建群组
      *
@@ -42,8 +42,7 @@ class Group
      *
      * @return string
      */
-    public function create(CreateGroupItem $item): string
-    {
+    public function create(CreateGroupItem $item): string {
         $r = $this->httpClient->postJson('group_open_http_svc/create_group',
             (array)$item
         );
@@ -53,13 +52,12 @@ class Group
     /**
      * 获取群组资料
      *
-     * @param string                   $groupId
+     * @param string $groupId
      * @param GroupResponseFilter|null $filter
      *
      * @return array
      */
-    public function get(string $groupId, GroupResponseFilter $filter = null): array
-    {
+    public function get(string $groupId, GroupResponseFilter $filter = null): array {
         $args = [
             'GroupIdList' => [$groupId]
         ];
@@ -75,13 +73,12 @@ class Group
     /**
      * 批量获取群组资料
      *
-     * @param array                    $groupIds
+     * @param array $groupIds
      * @param GroupResponseFilter|null $filter
      *
      * @return array
      */
-    public function batchGet(array $groupIds, GroupResponseFilter $filter = null): array
-    {
+    public function batchGet(array $groupIds, GroupResponseFilter $filter = null): array {
         $args = [
             'GroupIdList' => $groupIds
         ];
@@ -93,13 +90,12 @@ class Group
     /**
      * 修改群基本信息
      *
-     * @param string          $groupId
+     * @param string $groupId
      * @param ModifyGroupItem $item
      *
      * @return bool
      */
-    public function modify(string $groupId, ModifyGroupItem $item): bool
-    {
+    public function modify(string $groupId, ModifyGroupItem $item): bool {
         $item->setGroupId($groupId);
         $r = $this->httpClient->postJson('group_open_http_svc/modify_group_base_info', (array)$item);
         return $r['ActionStatus'] === Constants::ACTION_STATUS_OK;
@@ -112,8 +108,7 @@ class Group
      *
      * @return bool
      */
-    public function destroy(string $groupId): bool
-    {
+    public function destroy(string $groupId): bool {
         $r = $this->httpClient->postJson('group_open_http_svc/destroy_group', ['GroupId' => $groupId]);
         return $r['ActionStatus'] === Constants::ACTION_STATUS_OK;
     }
@@ -127,8 +122,7 @@ class Group
      *
      * @return bool
      */
-    public function changeGroupOwner(string $groupId, string $newOwnerAccountId): bool
-    {
+    public function changeGroupOwner(string $groupId, string $newOwnerAccountId): bool {
         $r = $this->httpClient->postJson('group_open_http_svc/change_group_owner', [
             'GroupId' => $groupId, 'NewOwner_Account' => $newOwnerAccountId
         ]);
@@ -140,12 +134,11 @@ class Group
      * 拉取到的成员角色，包括：Owner(群主)，Admin(群管理员)，Member(普通群成员），NotMember(非群成员)
      *
      * @param string $groupId
-     * @param array  $accountIds 表示需要查询的用户帐号，最多支持500个帐号
+     * @param array $accountIds 表示需要查询的用户帐号，最多支持500个帐号
      *
      * @return array
      */
-    public function getRoleInGroup(string $groupId, array $accountIds): array
-    {
+    public function getRoleInGroup(string $groupId, array $accountIds): array {
         if (count($accountIds) > 500) {
             throw new \InvalidArgumentException('AccountIds size limit exceeded.', -1);
         }
@@ -162,8 +155,7 @@ class Group
      *
      * @return array
      */
-    public function getJoinedGroupList(string $accountId): array
-    {
+    public function getJoinedGroupList(string $accountId): array {
         $p = [
             'Member_Account' => $accountId
         ];
@@ -175,17 +167,16 @@ class Group
      * 获取用户所加入的群组(分页)
      *
      * @param string $accountId
-     * @param int    $offset
-     * @param int    $limit
+     * @param int $offset
+     * @param int $limit
      *
      * @return array
      */
-    public function getJoinedGroupListByPage(string $accountId, int $offset, int $limit = 20): array
-    {
+    public function getJoinedGroupListByPage(string $accountId, int $offset, int $limit = 20): array {
         $p = [
             'Member_Account' => $accountId,
-            'Offset' => $offset,
-            'Limit' => $limit,
+            'Offset'         => $offset,
+            'Limit'          => $limit,
         ];
         $r = $this->httpClient->postJson('group_open_http_svc/get_joined_group_list', $p);
         return $r;
@@ -198,8 +189,7 @@ class Group
      *
      * @return bool
      */
-    public function modifyGroupMemberInfo(ModifyGroupMemberInfoItem $item): bool
-    {
+    public function modifyGroupMemberInfo(ModifyGroupMemberInfoItem $item): bool {
         $r = $this->httpClient->postJson('group_open_http_svc/modify_group_member_info', (array)$item);
         return $r['ActionStatus'] === Constants::ACTION_STATUS_OK;
     }
@@ -208,17 +198,18 @@ class Group
      * 增加群成员
      *
      * @param string $groupId
-     * @param array  $accountIds
-     * @param bool   $silence
+     * @param array $accountIds
+     * @param bool $silence
      *
      * @return array
      */
-    public function addGroupMember(string $groupId, array $accountIds, bool $silence = true): array
-    {
+    public function addGroupMember(string $groupId, array $accountIds, bool $silence = true): array {
         $r = $this->httpClient->postJson('group_open_http_svc/add_group_member', [
-            'GroupId' => $groupId,
-            'MemberList' => array_map(function ($v) { return ['Member_Account' => $v]; }, $accountIds),
-            'Silence' => $silence ? 1 : 0
+            'GroupId'    => $groupId,
+            'MemberList' => array_map(function ($v) {
+                return ['Member_Account' => $v];
+            }, $accountIds),
+            'Silence'    => $silence ? 1 : 0
         ]);
         return $r['MemberList'];
     }
@@ -228,25 +219,25 @@ class Group
      * 传入数组成员，如果在群里都不存在会返回memberlist is empty的错误
      *
      * @param string $groupId
-     * @param array  $accountIds
+     * @param array $accountIds
      * @param string $reason
-     * @param bool   $silence
+     * @param bool $silence
      *
      * @return bool
      */
     public function deleteGroupMember(
         string $groupId,
-        array $accountIds,
+        array  $accountIds,
         string $reason = '',
-        bool $silence = false
+        bool   $silence = false
     ): bool {
         if (count($accountIds) > 500) {
             throw new \InvalidArgumentException('AccountIds size limit exceeded.', -1);
         }
         $p = [
-            'GroupId' => $groupId,
+            'GroupId'             => $groupId,
             'MemberToDel_Account' => $accountIds,
-            'Silence' => $silence ? 1 : 0
+            'Silence'             => $silence ? 1 : 0
         ];
         empty($reason) or $p['Reason'] = $reason;
         $r = $this->httpClient->postJson('group_open_http_svc/delete_group_member', $p);
@@ -256,13 +247,12 @@ class Group
     /**
      * 获取群成员详细资料
      *
-     * @param string                             $groupId
+     * @param string $groupId
      * @param GroupMemberInfoResponseFilter|null $filter
      *
      * @return array
      */
-    public function getGroupMemberInfo(string $groupId, GroupMemberInfoResponseFilter $filter = null): array
-    {
+    public function getGroupMemberInfo(string $groupId, GroupMemberInfoResponseFilter $filter = null): array {
         $p = ['GroupId' => $groupId,];
         if (null !== $filter) {
             $p += (array)$filter;
@@ -274,17 +264,17 @@ class Group
     /**
      * 获取群成员详细资料(分页)
      *
-     * @param string                             $groupId
-     * @param int                                $offset
-     * @param int                                $limit
+     * @param string $groupId
+     * @param int $offset
+     * @param int $limit
      * @param GroupMemberInfoResponseFilter|null $filter
      *
      * @return array
      */
     public function getGroupMemberInfoByPage(
-        string $groupId,
-        int $offset,
-        $limit = 100,
+        string                        $groupId,
+        int                           $offset,
+                                      $limit = 100,
         GroupMemberInfoResponseFilter $filter = null
     ): array {
         $p = ['GroupId' => $groupId, 'Offset' => $offset, 'Limit' => $limit];
@@ -292,6 +282,73 @@ class Group
             $p += (array)$filter;
         }
         $r = $this->httpClient->postJson('group_open_http_svc/get_group_member_info', $p);
+        return $r;
+    }
+
+
+    /**
+     * 获取群成员详细资料 需要旗舰版资源包配合使用
+     *
+     * @param string $groupId
+     * @param GroupMemberInfoResponseFilter|null $filter
+     *
+     * @return array
+     */
+    public function getAvGroupMemberList(string $groupId, $timestamp = 0, $mark = null): array {
+        $p = [
+            'GroupId'   => $groupId,
+            'Timestamp' => $timestamp
+        ];
+        if (null !== $mark) {
+            $p["Mark"] += $mark;
+        }
+        $r = $this->httpClient->postJson('group_open_avchatroom_http_svc/get_members', $p);
+        return $r;
+    }
+
+    /**
+     * 直播群封禁成员列表  需要旗舰版资源包配合使用
+     * @return array
+     */
+    public function getAvBanMemberList($groupId,$offset=0,$limit=200) {
+        $p = [
+            'GroupId'   => $groupId,
+            'Offset' => $offset
+        ];
+        if (null !== $limit) {
+            $p["Limit"] += $limit;
+        }
+        $r = $this->httpClient->postJson('group_open_http_svc/get_group_ban_member', $p);
+        return $r;
+    }
+
+    /**
+     * 封禁语音房成员 需要旗舰版资源包配合使用
+     * @return array
+     */
+    public function setAvBanMember($groupId,array $member_list, int $duration, $description=null) {
+        $p = [
+            'GroupId'   => $groupId,
+            'Members_Account' => $member_list,
+            "Duration" => $duration,
+        ];
+        if (null !== $description){
+            $p["Description"] = $description;
+        }
+        $r = $this->httpClient->postJson('group_open_http_svc/ban_group_member',$p);
+        return $r;
+    }
+
+    /**
+     * 解封语音房成员 需要旗舰版资源包配合使用
+     * @return array
+     */
+    public function setAvUnbanMember($groupId,array $member_list) {
+        $p = [
+            'GroupId'   => $groupId,
+            'Members_Account' => $member_list,
+        ];
+        $r = $this->httpClient->postJson('group_open_http_svc/ban_group_member',$p);
         return $r;
     }
 
